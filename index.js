7ce4668dc7e7d8015eaa5f2d89d1309a3cda717a
Michelle David <rachaelholy@gmail.com>
2022-12-10 16:55:09 +0100

    shuffle the cards when draw button is clicked

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-let cardId
+let card_Id
 
 document.getElementById('new-game').addEventListener('click', () => {
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
        .then(res => res.json())
        .then(data => {
         console.log(data)
-        cardId = data.deck_id
+        card_Id = data.deck_id
        })
        
 
 })
+ let drawCard = () => {
+    fetch(`https://deckofcardsapi.com/api/deck/${card_Id}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => {
+     console.log(data.cards)
+     document.getElementById('draw-img').innerHTML = 
+     `<img src=${data?.cards[0].image}/>
+      <img src=${data?.cards[1].image}/>
+     `
+    })
+ }
+document.getElementById('draw-btn').addEventListener('click', drawCard)
