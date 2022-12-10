88bd8aebd84fbf75a1a92d07be7928e0062ba5b1
Michelle David <rachaelholy@gmail.com>
2022-12-10 17:43:30 +0100

    fix bug from GET 404 error. Added comments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let card_Id
-
+// fetch the API to start a new game when clicked
 document.getElementById('new-game').addEventListener('click', () => {
-    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
+    fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
        .then(res => res.json())
        .then(data => {
         console.log(data)
@@ -10,14 +10,15 @@ document.getElementById('new-game').addEventListener('click', () => {
        
 
 })
- let drawCard = () => {
-    fetch(`https://deckofcardsapi.com/api/deck/${card_Id}/draw/?count=2`)
+// Draw and shuffle the cards when clicked 
+ const drawCard = () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${card_Id}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {
      console.log(data.cards)
      document.getElementById('draw-img').innerHTML = 
-     `<img src=${data?.cards[0].image}/>
-      <img src=${data?.cards[1].image}/>
+     `<img src=${data.cards[0].image} />
+      <img src=${data.cards[1].image} />
      `
     })
  }
