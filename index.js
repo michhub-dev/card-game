270211caa0472c0c38e62d0bc47f86802df10a4a
Michelle David <rachaelholy@gmail.com>
2022-12-11 17:04:04 +0100

    defined logic to determine the winning card

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ document.getElementById('new-game').addEventListener('click', () => {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
        .then(res => res.json())
        .then(data => {
-        console.log(data)
         card_Id = data.deck_id
        })
        
@@ -22,3 +21,27 @@ document.getElementById('new-game').addEventListener('click', () => {
     })
  }
 document.getElementById('draw-btn').addEventListener('click', drawCard)
+
+const determineWinner = (card1, card2) => {
+    const options = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1Value = options.indexOf(card1.value) 
+    const card2Value = options.indexOf(card2.value) 
+
+    if(card1Value > card2Value){
+        console.log('card 1 wins')
+    }else if(card1Value < card2Value){
+        console.log('card 2 wins')
+    }else{
+        console.log('draw')
+    }
+}
+
+const card1IndexValue = {
+    value: '8'
+}
+const card2IndexValue = {
+    value: 'ACE'
+}
+
+determineWinner(card1IndexValue, card2IndexValue)
